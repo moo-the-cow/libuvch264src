aea47139422c91f8a1299216b80205bf660e8390
moo <34907770+moo-the-cow@users.noreply.github.com>
2026-05-16 17:39:39 +0200

    Update gstlibuvch264src.h

--- libuvch264src/src/gstlibuvch264src.h
+++ libuvch264src/src/gstlibuvch264src.h
@@ -1,7 +1,6 @@
 #ifndef GST_LIBUVC_H264_SRC_H
 #define GST_LIBUVC_H264_SRC_H
 
-#include <glib.h>
 #include <gst/gst.h>
 #include <gst/base/gstpushsrc.h>
 #include <libuvc/libuvc.h>
@@ -15,33 +14,44 @@ G_DECLARE_FINAL_TYPE(GstLibuvcH264Src, gst_libuvc_h264_src, GST, LIBUVC_H264_SRC
 #define TIMEOUT_DURATION G_TIME_SPAN_SECOND // 1 second
 #define DJI_VENDOR_ID 0x2ca3
 #define DJI_PRODUCT_ID 0x0023
-
 #define SPSPPSBUFSZ 1024
-
 #define MIN_FRAMES_CALC_INTERVAL 60
 
 struct _GstLibuvcH264Src {
   GstPushSrc parent_instance;
+
   gchar* index;
   uvc_context_t *uvc_ctx;
   uvc_device_t *uvc_dev;
   uvc_device_handle_t *uvc_devh;
   uvc_stream_ctrl_t uvc_ctrl;
+  enum uvc_frame_format frame_format;
+
   GAsyncQueue *frame_queue;
   gboolean streaming;
-  GstClockTime uvc_start_time;
+
+  /* --- clock / PTS fields --- */
+  GstClock *clock;
+  int64_t pts_offset_sum;
+  int64_t pts_stretch;
+  GstClockTime base_time;
+
   GstClockTime prev_pts;
   gint64 frame_interval; // in ns
   guint64 prev_int_ts;
   gint frame_count;
   gboolean had_idr;
   gboolean send_sps_pps;
+
+  /* --- VPS support --- */
+  gint vps_length;
   gint sps_length;
   gint pps_length;
+  unsigned char vps[SPSPPSBUFSZ];
   unsigned char sps[SPSPPSBUFSZ];
   unsigned char pps[SPSPPSBUFSZ];
-  
-  // Control socket additions
+
+  /* --- moo-the-cow control socket additions --- */
   gint control_socket;
   gpointer control_thread;
   gboolean control_running;
